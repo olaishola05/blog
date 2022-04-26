faff5ea86b1bb8cd80267be4053722c264bc74c7
Ola Ishola <olaishola@hotmail.co.uk>
2022-04-26 09:17:14 +0100

    Add Nested routes for posts
    Refactor routes and use resources for proper routes structuring

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,9 @@
 class UsersController < ApplicationController
     def index 
-        render 'index'
+        
     end
 
     def show
-        params[:id]
-
-        puts params
-        render show
+        
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-
-  get '/users/index'
-
-  get '/users/:id', to: 'users#show'
   
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
 
   root  'users#index'
 end
