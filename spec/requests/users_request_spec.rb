cb612482c3673f1c0c05bb91977c8cc410fff356
Ola Ishola <olaishola@hotmail.co.uk>
2022-04-26 12:42:20 +0100

    Add more test for User controller

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -5,10 +5,32 @@ RSpec.describe 'Users', type: :request do
         before (:example) { get users_path } # get(:index) 
         it 'return response status 200 or ok' do
             expect(response).to have_http_status(:success)
-            expect(response).to have_http_status(200)
+            expect(response.status).to eq(200)
             expect(response).to have_http_status(:ok)
         end
 
-       
+        it 'should render template index' do
+            expect(response).to render_template('index')
+        end
+
+        it 'Should return response body text' do
+            expect(response.body).to include('Users! Homepage')
+        end
+    end
+
+    describe 'GET #show' do
+        before(:example) { get user_path(1) } # get(:show, params: { id: 1 })
+
+        it 'return response status 200 or ok' do
+            expect(response).to have_http_status(:success)
+        end
+
+        it 'Should return response body text' do
+            expect(response.body).to include('A user homepage')
+        end
+
+        it 'Should render template show' do
+            expect(response).to render_template('show')
+        end
     end
 end
