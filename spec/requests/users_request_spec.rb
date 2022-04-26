429f498da01a1675a539404346f1c0e1f4833daf
Ola Ishola <olaishola@hotmail.co.uk>
2022-04-26 13:51:02 +0100

    Refactor tests and run linters

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,36 +1,37 @@
 require './rails_helper'
 
 RSpec.describe 'Users', type: :request do
-    describe 'GET #index' do
-        before (:example) { get users_path } # get(:index) 
-        it 'return response status 200 or ok' do
-            expect(response).to have_http_status(:success)
-            expect(response.status).to eq(200)
-            expect(response).to have_http_status(:ok)
-        end
+  describe 'GET #index' do
+    before(:example) { get users_path } # get(:index)
 
-        it 'should render template index' do
-            expect(response).to render_template('index')
-        end
-
-        it 'Should return response body text' do
-            expect(response.body).to include('Users! Homepage')
-        end
+    it 'return response status 200 or ok' do
+      expect(response).to have_http_status(:success)
+      expect(response.status).to eq(200)
+      expect(response).to have_http_status(:ok)
     end
 
-    describe 'GET #show' do
-        before(:example) { get user_path(1) } # get(:show, params: { id: 1 })
-
-        it 'return response status 200 or ok' do
-            expect(response).to have_http_status(:success)
-        end
-
-        it 'Should return response body text' do
-            expect(response.body).to include('A user homepage')
-        end
-
-        it 'Should render template show' do
-            expect(response).to render_template('show')
-        end
+    it 'should render template index' do
+      expect(response).to render_template('index')
     end
+
+    it 'Should return response body text' do
+      expect(response.body).to include('Users! Homepage')
+    end
+  end
+
+  describe 'GET #show' do
+    before(:example) { get user_path(1) } # get(:show, params: { id: 1 })
+
+    it 'return response status 200 or ok' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'Should return response body text' do
+      expect(response.body).to include('A user homepage')
+    end
+
+    it 'Should render template show' do
+      expect(response).to render_template('show')
+    end
+  end
 end
