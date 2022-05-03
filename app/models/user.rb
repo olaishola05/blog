0dbb5d3e96886a57fd7e2eab80999de12bc449ba
Ola Ishola <olaishola@hotmail.co.uk>
2022-05-03 13:27:29 +0100

    Add method to return recent posts from a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ApplicationRecord
     has_many :posts
     has_many :comments, through: :posts
     has_many :likes, through: :posts
+
+
+    def recent_posts
+        @user = User.find(params[:id])
+        @posts = @user.posts.all.order(created_at: :desc).limit(3)
+    end
 end
