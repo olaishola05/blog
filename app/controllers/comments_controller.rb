77abcdf9cf7ebe0dd059456aecf105d6ba43d1c8
Ola Ishola <olaishola@hotmail.co.uk>
2022-05-06 23:25:24 +0100

    Add controller for comments & actions to create new comments

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,23 @@
+class CommentsController < ApplicationController
+    def new
+        @comment = Comment.new
+    end
+
+    def create
+        @comment = Comment.new(comment_params)
+        @post = Post.find(params[:post_id])
+        @comment.user_id = current_user.id
+        @post.post_id = @post.id
+
+        if @comment.save
+            redirect_to user_post_path(user_id: @post.user_id, id: @post.id)
+        else
+            render :new, alert: 'An error has occurred while creating the comment'
+        end
+    end
+
+    private
+    def comment_params
+        params.require(:comment).permit(:text)
+    end
+end
