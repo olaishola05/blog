682cd0945d5eaa61fc06e5dd70ac495c585217f1
Ola Ishola <olaishola@hotmail.co.uk>
2022-05-07 15:51:45 +0100

    Add new post & like functionalities

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,23 +1,27 @@
 class CommentsController < ApplicationController
-    def new
-        @comment = Comment.new
-    end
+  def new
+    @comment = Comment.new
+  end
 
-    def create
-        @comment = Comment.new(comment_params)
-        @post = Post.find(params[:post_id])
-        @comment.user_id = current_user.id
-        @post.post_id = @post.id
+  def create
+    # @comment = Comment.new(comment_params)
+    @post = Post.find(params[:post_id])
+    @comment = current_user.comments.new(
+      text: comment_params[:text],
+      user_id: params[:user_id],
+      post_id: @post.id
+    )
 
-        if @comment.save
-            redirect_to user_post_path(user_id: @post.user_id, id: @post.id)
-        else
-            render :new, alert: 'An error has occurred while creating the comment'
-        end
+    if @comment.save
+      redirect_to user_post_path(user_id: @post.user_id, id: @post.id)
+    else
+      render :new, alert: 'An error has occurred while creating the comment'
     end
+  end
 
-    private
-    def comment_params
-        params.require(:comment).permit(:text)
-    end
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
 end
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,18 +1,20 @@
 class LikesController < ApplicationController
-    def new
-        @like = Like.new
-    end
+  def new
+    @like = Like.new
+  end
 
-    def create
-        @post = Post.find(params[:post_id])
-        @user = current_user
-        @like = Like.new(user_id: @user.id, post_id: @post.id)
+  def create
+    @post = Post.find(params[:post_id])
+    @user = current_user
+    @like = Like.new(user_id: @user.id, post_id: @post.id)
 
-        if @like.save
-            @post = Post.find(params[:post_id])
-            redirect_to user_post_path(user_id: @post.user_id, id: @post.id)
-        else
-            redirect_to user_post_path(user_id: @post.user_id, id: @post.id), alert: "An error occurr while trying to like this post"
-        end
+    if @like.save
+      @post = Post.find(params[:post_id])
+      @like.like_count(@post.id)
+      redirect_to user_post_path(user_id: @post.user_id, id: @post.id)
+    else
+      redirect_to user_post_path(user_id: @post.user_id, id: @post.id),
+                  alert: 'An error occurr while trying to like this post'
     end
+  end
 end
