4f60c9b0ca3aba278eed61467363a1976885795a
Ola Ishola <olaishola@hotmail.co.uk>
2022-05-09 20:46:10 +0100

    remove confirmable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,18 @@
 class ApplicationController < ActionController::Base
-  add_flash_types :success, :error
+  protect_from_forgery with: :exception
+  before_action :authenticate_user!
 
-  # def current_user
-  #   User.first
-  # end
-  # helper_method :current_user
+  before_action :update_allowed_parameters, if: :devise_controller?
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:email, :name, :photo, :bio, :posts_counter, :password, :password_confirmation)
+    end
+
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:email, :name, :photo, :bio, :password, :password_confirmation, :current_password)
+    end
+  end
 end
