43f4451cb5b7959a06f38df66405a3e56c5670ed
Ola Ishola <olaishola@hotmail.co.uk>
2022-05-09 13:08:03 +0100

    Add devise to gem and run bundle install

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   add_flash_types :success, :error
 
-  def current_user
-    User.first
-  end
-  helper_method :current_user
+  # def current_user
+  #   User.first
+  # end
+  # helper_method :current_user
 end
