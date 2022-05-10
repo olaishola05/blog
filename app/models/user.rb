e4048357be5e5f709938f69646f9dbedc6083caa
Ola Ishola <olaishola@hotmail.co.uk>
2022-05-10 21:57:04 +0100

    Fix posts_counter error & restyle nav

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
   validates :name, presence: true
-  # validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
+  validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
 
   has_many :posts
   has_many :comments
