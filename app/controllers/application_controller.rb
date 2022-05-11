a151da54216398f1f2a50512efaf2caf732f353f
Ola Ishola <olaishola@hotmail.co.uk>
2022-05-11 12:49:11 +0100

    Upadte user ability

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
+  load_and_authorize_resource
+
   before_action :update_allowed_parameters, if: :devise_controller?
 
   protected
