f8b1080cb9c09274ad22c7ec7d805c3c227ec2e0
MeqdamAlqudah <memuq56@gmail.com>
2022-05-11 15:37:10 +0300

    Add update sllowed parameters method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,15 @@ class ApplicationController < ActionController::Base
 
   before_action :update_allowed_parameters, if: :devise_controller?
 
+  protected
 
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:email, :name, :photo, :bio, :posts_counter, :password, :password_confirmation)
+    end
+
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:email, :name, :photo, :bio, :password, :password_confirmation, :current_password)
+    end
+  end
 end
