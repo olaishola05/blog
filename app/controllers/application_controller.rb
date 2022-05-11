ad6719446c6f7fba2c888e71396e79691b42eeca
Ola Ishola <olaishola@hotmail.co.uk>
2022-05-11 17:05:58 +0100

    Broken app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :update_allowed_parameters, if: :devise_controller?
@@ -11,7 +12,7 @@ class ApplicationController < ActionController::Base
 
   def update_allowed_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |u|
-      u.permit(:email, :name, :photo, :bio, :posts_counter, :password, :password_confirmation)
+      u.permit(:email, :name, :photo, :bio, :posts_counter, :role, :password, :password_confirmation)
     end
 
     devise_parameter_sanitizer.permit(:account_update) do |u|
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,24 +5,24 @@ class Ability
 
   def initialize(user)
       user ||= User.new # guest user (not logged in)
+      can :read, Post
       admin_privilege if user.is? :admin
-      else
-        can :read, Post
-        user_privilege if user.is? :default
-        manager_privilege if user.is? :manager
-        moderator_privilege if user.is? :moderator
-      end
+      user_privilege(user) if user.is? :default
+      manager_privilege if user.is? :manager
+      moderator_privilege if user.is? :moderator
   end
 
   private
 
-  def user_privilege
+  def user_privilege(user)
     can :manage, Post, user_id: user.id
     can :create, [Comment, Like]
+    can :update, [Post, Comment], user_id: user.id
+    can :destroy, [Post, Comment], user_id: user.id
   end
 
   def manager_privilege
-    can :manage , Comment
+    can :manage, Comment
   end
 
   def moderator_privilege
