b33a0a1771e77ae4bd07f1b8e0bace9ef1c40754
Ola Ishola <olaishola@hotmail.co.uk>
2022-05-11 12:54:39 +0100

    add Load and Authorize Resource to user & handle failure by adding rescue to application-controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
-
-  load_and_authorize_resource
-
   before_action :update_allowed_parameters, if: :devise_controller?
+
+  rescue_from CanCan::AccessDenied do | exception |
+    redirect_to root_url, alert: exception.message
+  end
 
   protected
 
